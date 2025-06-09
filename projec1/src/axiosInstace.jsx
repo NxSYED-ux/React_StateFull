e28017907bec937b18ce3ca4ae6d7e5c0e907cc1
Syed <syedmu729@gmail.com>
2025-06-09 18:17:42 +0500

    Initial Commit

--- projec1/src/axiosInstace.jsx
+++ projec1/src/axiosInstace.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5000',  // Set this to your backend's base URL
+  baseURL: 'http://localhost:7000',  // Set this to your backend's base URL
   withCredentials: true,  // To send cookies
 });
 
